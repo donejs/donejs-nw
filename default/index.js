df5a8577f03b5e9d472ea39e1ecb592551725d46
Kevin Phillips <kphillips86@gmail.com>
2015-10-18 22:50:02 -0500

    updating generator to update package.json to include nw.js properties

--- default/index.js
+++ default/index.js
@@ -1,49 +1,84 @@
 var generator = require('yeoman-generator');
 var ejs = require('ejs');
 var fs = require('fs');
+var Q = require('q');
+var _ = require('lodash');
 
 module.exports = generator.Base.extend({
   prompting: function () {
-    // var done = this.async();
-    // this.prompt([{
-    //   type    : 'input',
-    //   name    : 'name',
-    //   message : 'Name of project for nw',
-    //   default : this.appname // Default to current folder name
-    // }, {
-    //   type    : 'input',
-    //   name    : 'id',
-    //   message : 'ID of project for nw',
-    // }], function (answers) {
-    //   this.config.set('name', answers.name);
-    //   this.config.set('id', answers.id);
-    //   done();
-    // }.bind(this));
+    var done = this.async();
+    this.prompt([{
+      type    : 'input',
+      name    : 'main',
+      message : 'Main HTML file for your app',
+      default : 'production.html'
+    }, {
+      type    : 'input',
+      name    : 'width',
+      message : 'Width of application window',
+    }, {
+      type    : 'input',
+      name    : 'height',
+      message : 'Height of application window',
+    }], function (answers) {
+      this.config.set('main', answers.main);
+      this.config.set('width', answers.width);
+      this.config.set('height', answers.height);
+      done();
+    }.bind(this));
   },
   installingStealnw: function() {
     this.npmInstall(['steal-nw'], { 'saveDev': true });
   },
   writing: function () {
     var done = this.async();
-    var outputBuildjs = this.destinationPath('build.js');
+    var buildJsDeferred = Q.defer();
+    var packageJsonDeferred = Q.defer();
 
-    if (!this.fs.exists(outputBuildjs)) {
+    // update build.js
+    var buildJs = this.destinationPath('build.js');
+    if (!this.fs.exists(buildJs)) {
       this.fs.copyTpl(
         this.templatePath('build.ejs'),
-        outputBuildjs,
+        buildJs,
         {}
       );
-      done();
+      buildJsDeferred.resolve();
     } else {
-      fs.readFile(outputBuildjs, 'utf8', function(err, data) {
+      fs.readFile(buildJs, 'utf8', function(err, data) {
         if (data.indexOf('nwOptions') < 0) {
             var nwOptionsText = this.fs.read(this.templatePath('nwOptions.ejs'), 'utf8');
             var newContent = data + ejs.render(nwOptionsText, {});
-            fs.writeFile(outputBuildjs, newContent, done);
+            fs.writeFile(buildJs, newContent, function() {
+              buildJsDeferred.resolve();
+            });
         } else {
-          done();
+          buildJsDeferred.resolve();
         }
       }.bind(this));
     }
+
+    // update package.json
+    var packageJson = this.destinationPath('package.json');
+    fs.readFile(packageJson, 'utf8', function(err, data) {
+      var json = data && JSON.parse(data) || {};
+      json.main = this.config.get('main');
+      json.window = _.extend({}, json.window, {
+        width: this.config.get('width'),
+        height: this.config.get('height')
+      });
+      fs.writeFile(packageJson, JSON.stringify(json), function() {
+        packageJsonDeferred.resolve();
+      });
+    }.bind(this));
+
+    // complete writing once build.js and package.json are updated
+    Q.all([
+      buildJsDeferred.promise,
+      packageJsonDeferred.promise
+    ])
+    .then(function() {
+      done();
+    });
   }
 });
--- test/index.js
+++ test/index.js
@@ -4,52 +4,76 @@ var assert = require('yeoman-generator').assert;
 var fs = require('fs-extra');
 
 describe('donejs-nw', function() {
-  describe('when no build.js exists', function() {
+  describe('should create/update build.js', function() {
+    describe('when no build.js exists', function() {
+      before(function(done) {
+        helpers.run(path.join(__dirname, '../default'))
+          .on('end', done);
+      });
+
+      it('should write build.js', function() {
+        assert.file(['build.js']);
+        assert.fileContent('build.js', /steal-tools/);
+        assert.fileContent('build.js', /steal-nw/);
+      });
+    });
+
+    describe('when build.js was already created by generator-donejs', function() {
+      before(function(done) {
+        helpers.run(path.join(__dirname, '../default'))
+        .inTmpDir(function(dir) {
+          var done = this.async();
+          fs.copy(path.join(__dirname, 'templates/generator-donejs'), dir, done);
+        })
+        .on('end', done);
+      });
+
+      it('should add nwOptions to build.js', function() {
+        assert.file(['build.js']);
+        assert.fileContent('build.js', /generator-donejs build\.js/);
+        assert.fileContent('build.js', /steal-tools/);
+        assert.fileContent('build.js', /steal-nw/);
+      });
+    });
+
+    describe('when build.js was already created by generator-donejs and updated by donejs-cordvoa', function() {
+      before(function(done) {
+        helpers.run(path.join(__dirname, '../default'))
+        .inTmpDir(function(dir) {
+          var done = this.async();
+          fs.copy(path.join(__dirname, 'templates/donejs-nw'), dir, done);
+        })
+        .on('end', done);
+      });
+
+      it('should not overwrite build.js', function() {
+        assert.file(['build.js']);
+        assert.fileContent('build.js', /generator-donejs \+ donejs-nw build\.js/);
+        assert.fileContent('build.js', /steal-tools/);
+        assert.fileContent('build.js', /steal-nw/);
+      });
+    });
+  });
+
+  describe('should update package.json', function() {
     before(function(done) {
       helpers.run(path.join(__dirname, '../default'))
+        .inTmpDir(function(dir) {
+          var done = this.async();
+          fs.copy(path.join(__dirname, 'templates/package.json'), dir, done);
+        })
+        .withPrompts({
+          main: 'app.html',
+          width: '800',
+          height: '600'
+        })
         .on('end', done);
     });
 
-    it('should write build.js', function() {
-      assert.file(['build.js']);
-      assert.fileContent('build.js', /steal-tools/);
-      assert.fileContent('build.js', /steal-nw/);
-    });
-  });
-
-  describe('when build.js was already created by generator-donejs', function() {
-    before(function(done) {
-      helpers.run(path.join(__dirname, '../default'))
-      .inTmpDir(function(dir) {
-        var done = this.async();
-        fs.copy(path.join(__dirname, 'templates/generator-donejs'), dir, done);
-      })
-      .on('end', done);
-    });
-
-    it('should add nwOptions to build.js', function() {
-      assert.file(['build.js']);
-      assert.fileContent('build.js', /generator-donejs build\.js/);
-      assert.fileContent('build.js', /steal-tools/);
-      assert.fileContent('build.js', /steal-nw/);
-    });
-  });
-
-  describe('when build.js was already created by generator-donejs and updated by donejs-cordvoa', function() {
-    before(function(done) {
-      helpers.run(path.join(__dirname, '../default'))
-      .inTmpDir(function(dir) {
-        var done = this.async();
-        fs.copy(path.join(__dirname, 'templates/donejs-nw'), dir, done);
-      })
-      .on('end', done);
-    });
-
-    it('should not overwrite build.js', function() {
-      assert.file(['build.js']);
-      assert.fileContent('build.js', /generator-donejs \+ donejs-nw build\.js/);
-      assert.fileContent('build.js', /steal-tools/);
-      assert.fileContent('build.js', /steal-nw/);
+    it('with correct nw.js configuration', function() {
+      assert.file(['package.json']);
+      assert.JSONFileContent('package.json', { main: 'app.html' });
+      assert.JSONFileContent('package.json', { window: { width: 800, height: 600 } });
     });
   });
 });
